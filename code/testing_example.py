14afbee4c5b772da86a88d72390e4fc304e68e6c
shepai <dextershepherd@hotmail.co.uk>
2025-09-03 13:18:43 +0000

    additions

--- code/testing_example.py
+++ code/testing_example.py
@@ -4,16 +4,25 @@ sys.path.append('/its/home/drs25/Tactile_Language_Model/Library/')
 from TLM import *
 import numpy as np
 import pickle 
+import re
+def round_numbers_in_text(text, decimals=2):
+    def repl(match):
+        num = float(match.group())
+        return f"{num:.{decimals}f}"
+    return re.sub(r"\d+\.?\d*", repl, text)
 
 X=np.load("/mnt/data0/drs25/data/gelsight_language/X_data.npy")
 with open("/mnt/data0/drs25/data/gelsight_language/y_data.pkl", "rb") as file:
     y = pickle.load(file)
-
+y = [round_numbers_in_text(s, 2) for s in y]
 print(X.shape)
 print(len(y))
 
 tlm = TLM()
-tlm.load("/its/home/drs25/Tactile_Language_Model/data/gelsight_model")
-image=X[0].reshape((1,1,*X[0].shape))
-print(image.shape)
-print("generated:",tlm.generate_caption(image))
+tlm.load("/its/home/drs25/Tactile_Language_Model/data/gelsight_model_100")
+for i in range(10):
+    print("*****************")
+    image=X[0].reshape((1,1,*X[0].shape))
+    print(image.shape)
+    print("\tGenerated:",tlm.generate_caption(image))
+    print("\tExpected:",y[0])
--- code/training_example.py
+++ code/training_example.py
@@ -4,10 +4,17 @@ sys.path.append('/its/home/drs25/Tactile_Language_Model/Library/')
 from TLM import *
 import numpy as np
 import pickle 
+import re
+def round_numbers_in_text(text, decimals=2):
+    def repl(match):
+        num = float(match.group())
+        return f"{num:.{decimals}f}"
+    return re.sub(r"\d+\.?\d*", repl, text)
 
 X=np.load("/mnt/data0/drs25/data/gelsight_language/X_data.npy")
 with open("/mnt/data0/drs25/data/gelsight_language/y_data.pkl", "rb") as file:
     y = pickle.load(file)
+y = [round_numbers_in_text(s, 2) for s in y]
 
 print(X.shape)
 print(len(y))
@@ -16,7 +23,7 @@ train_y=y[0:len(y)//2]
 del X
 del y
 tlm = TLM()
-tlm.train(train_X,train_y,epochs=10,save="/its/home/drs25/Tactile_Language_Model/data/gelsight_model_10")
+tlm.train(train_X,train_y,epochs=50,save="/its/home/drs25/Tactile_Language_Model/data/gelsight_model_50",lr=0.01)
 #tlm.save("/its/home/drs25/Tactile_Language_Model/data/gelsight_model_10")
 image=X[0].reshape((1,1,*X[0].shape))
 print(image.shape)
