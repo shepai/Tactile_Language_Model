a5253dfbccf61c82b5a48865b374cbbf1795442f
shepai <dextershepherd@hotmail.co.uk>
2025-09-08 12:40:12 +0000

    u[pdates

--- code/training_example.py
+++ code/training_example.py
@@ -12,18 +12,18 @@ def round_numbers_in_text(text, decimals=2):
     return re.sub(r"\d+\.?\d*", repl, text)
 
 X=np.load("/mnt/data0/drs25/data/gelsight_language/X_data.npy")
-with open("/mnt/data0/drs25/data/gelsight_language/y_data.pkl", "rb") as file:
+with open("/mnt/data0/drs25/data/gelsight_language/y_json.pkl", "rb") as file:
     y = pickle.load(file)
-y = [round_numbers_in_text(s, 2) for s in y]
 
 print(X.shape)
 print(len(y))
 train_X=X[0:len(X)//2]
 train_y=y[0:len(y)//2]
+
 del X
 del y
 tlm = TLM()
-tlm.train(train_X,train_y,epochs=50,save="/its/home/drs25/Tactile_Language_Model/data/gelsight_model_50",lr=0.01)
+tlm.train(train_X,train_y,epochs=1500,save="/its/home/drs25/Tactile_Language_Model/data/gelsight_model_1500")
 #tlm.save("/its/home/drs25/Tactile_Language_Model/data/gelsight_model_10")
 image=X[0].reshape((1,1,*X[0].shape))
 print(image.shape)
